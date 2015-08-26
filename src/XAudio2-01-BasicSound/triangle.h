76bbf3ccd6a2eb5780c06d95a1a88bb045902e26
byhj <476652422@qq.com>
2015-08-26 14:28:08 +0800

    add xaudio2 tutorial 01

---
+++ src/XAudio2-01-BasicSound/triangle.h
@@ -0,0 +1,53 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+#include <d3d11.h>
+#include <DirectXMath.h>
+
+#include "d3d/Shader.h"
+#include "d3d/Utility.h"
+
+using namespace DirectX;
+
+namespace byhj
+{
+
+class Triangle
+{
+
+public:
+	Triangle();
+	~Triangle();
+
+public:
+	void Update();
+	void Init(ID3D11Device *pD3D11Device, ID3D11DeviceContext *pD3D11DeviceContext);
+	void Render(ID3D11DeviceContext *pD3D11DeviceContext, const d3d::MatrixBuffer &matrix);
+	void Shutdown();
+
+private:
+	void init_buffer(ID3D11Device *pD3D11Device, ID3D11DeviceContext *pD3D11DeviceContext);
+	void init_shader(ID3D11Device *pD3D11Device);
+
+	struct Vertex
+	{
+		XMFLOAT3 Position;
+	};
+
+	ID3D11InputLayout  *m_pInputLayout       = nullptr;
+	ID3D11VertexShader *m_pVS                = nullptr;
+	ID3D11PixelShader  *m_pPS                = nullptr;
+	ID3D11Buffer       *m_pVertexBuffer      = nullptr;
+	ID3D11Buffer       *m_pIndexBuffer       = nullptr;
+	ID3D11Buffer       *m_pMVPBuffer         = nullptr;
+
+	int m_IndexCount = 0;
+	int m_VertexCount = 0;
+	d3d::MatrixBuffer cbMatrix;
+	d3d::Shader TriangleShader;
+};
+
+
+}
+
+#endif
